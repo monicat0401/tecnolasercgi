e771218cfdfcc7c1e3970cf67648b0b3bbcda558
monica torres <monicatorres.1995@hotmail.com>
2018-05-27 21:59:33 -0500

    contacto completo
    completo

--- enviarate.php
+++ enviarate.php
@@ -7,7 +7,7 @@
  $telefonodos= $_POST["telefonodos"];
  $mensajedos= $_POST["mensajedos"];
 
- $contenido = "Nombre: " . $nombredos . "\ntipodemensaje: " . $info .  "\nEmail: " . $emaildos . "\nTelefono: " . $telefonodos . "\nmensaje: " . $mensajedos;
+ $contenido = "Nombre: " . $nombredos . "\nTipo de Mensaje: " . $info .  "\nEmail: " . $emaildos . "\nTelefono: " . $telefonodos . "\nMensaje: " . $mensajedos;
 
  mail($destino,"Contacto", utf8_decode($contenido));
  header("location:contacto.html");
